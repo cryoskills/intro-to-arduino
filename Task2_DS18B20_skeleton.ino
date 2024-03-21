2d6ee17d92ada385616f48fa9b3dc5a504c19a4d
EmmaFisher17 <157727590+EmmaFisher17@users.noreply.github.com>
2024-03-21 11:14:06 +0000

    Add files via upload

---
+++ Task2_DS18B20_skeleton.ino
@@ -0,0 +1,38 @@
+/* Cryoskills Introduction to Arduino
+Task 2 - DS18B20 Digital Temperature Sensor
+Skeleton Code */
+
+// Include libraries:
+
+
+// initalise the digital temperature probe:
+DS18B20 ds(5); // DS18B20 is connected to pin 5 (D5)
+
+// initialise variables:
+float ds_temp; // DS probe temperature measurement, value is a float
+
+// initialise functions:
+float get_ds_temp();   // fucntion to get a digital temperature, return it as a float
+
+void setup() {
+  Serial.begin(9600); // sets baud rate (serial communication at 9600 bits of data per second)
+}
+
+void loop() {
+  // get temperature value from the DS probe and print to serial  
+  while (ds.selectNext()) {
+    Serial.print("Temperature: ");
+    Serial.print(ds.getTempC());
+    Serial.println(" deg C");
+  }
+  
+  // if temperature is greater than 22 degrees C
+  // turn on the LED
+  // if temperature is less than 22 degrees C
+  // turn the LED off
+
+  delay(1000); // wait 1 second
+}
+ 
+// get temperature value from DS probe as a function here:
+// YOUR CODE HERE...
